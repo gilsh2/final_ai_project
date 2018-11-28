fe3e61c03ae05284e85b6c59a3091913c93c9373
Gil Lapid <gilsh@microsoft.com>
2018-11-28 08:14:17 -0800

    split to multiple .py files

--- mastermind_ai_train.py
+++ mastermind_ai_train.py
@@ -2,32 +2,21 @@ import mastermind_ai_core
 
 
 nn = NNStrategy()    
-nn.TheModel = torch.load("bestavg_sofar_4394.model")
+nn.TheModel = torch.load("bestavg_sofar_43865.model")
 nn.TheModel.eval()
-fname = "rrr2.txt"
+fname = "tr.txt"
 count = 0
-while(True):   
-    #Tree = StrategyTreeBuilder.Build(KnuthStrategy,allcombinations,allcombinations)  
-       
-    random.shuffle(allcombinations)
-    #Tree = StrategyTreeBuilder.Build(KnuthStrategy(),allcombinations,allcombinations) 
-    Tree = StrategyTreeBuilder.Build(KnuthStrategy(),allcombinations,allcombinations) 
-    count = count +1
-  
+while(True):    
+    Tree = StrategyTreeBuilder.Build(nn,allcombinations,allcombinations) 
+    count = count +1  
     stat= MasterMindSolverSimulator.Simulate(Tree,1,fname)
     print (stat)
    
-    
+    random.shuffle(allcombinations)
   
-    if( (avgsteps == None or avgsteps > stat[1]) and  stat[0] == 5  ):
+    if(avgsteps == None or avgsteps > stat[1]) :
         avgsteps =  stat[1]    
         bestavg = stat
         torch.save(nn.TheModel,"bestavg_sofar.model")
-        StrategyTreeBuilder.Save(Tree,"knuth_tree_optimized.txt")
-    
-   
-    
+                  
     print("progress so far ",(bestavg))
-    #nn.Train(fname)  
-    #solver=MasterMindSolver(allcombinations,Tree)
-    #solver.Play([Color.RED,Color.RED,Color.RED,Color.RED])
